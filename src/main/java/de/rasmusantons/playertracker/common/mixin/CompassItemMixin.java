69b2faa649e49a7b11b043ca13ae58036350d2ad
rasmus <mail@rasmusantons.de>
2024-05-20 18:54:35 +0200

    prevent tracker being used on lodestone

--- src/main/java/de/rasmusantons/playertracker/common/mixin/CompassItemMixin.java
+++ src/main/java/de/rasmusantons/playertracker/common/mixin/CompassItemMixin.java
@@ -1,19 +1,22 @@
-package de.rasmusantons.playertracker.server.mixin;
+package de.rasmusantons.playertracker.common.mixin;
 
 import de.rasmusantons.playertracker.Utils;
 import de.rasmusantons.playertracker.server.extension.ServerPlayerExtension;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.GlobalPos;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.CompassItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.LodestoneTracker;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
@@ -32,6 +35,13 @@ public class CompassItemMixin {
             BlockPos blockPos = nowTracking.getOnPos();
             LodestoneTracker lodestoneTracker = new LodestoneTracker(Optional.of(GlobalPos.of(nowTracking.level().dimension(), blockPos)), false);
             itemStack.set(LODESTONE_TRACKER, lodestoneTracker);
+        }
+    }
+
+    @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
+    private void onUseOn(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        if (Utils.isPlayerTracker(context.getItemInHand())) {
+            cir.setReturnValue(InteractionResult.PASS);
         }
     }
 }
