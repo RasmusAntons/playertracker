53b9354dd765214fbd5e29e5e3cd295aa95e3c4f
rasmus <mail@rasmusantons.de>
2025-04-13 20:29:29 +0200

    remove duplicate package receiver registration

--- src/main/java/de/rasmusantons/playertracker/server/PlayerTrackerServer.java
+++ src/main/java/de/rasmusantons/playertracker/server/PlayerTrackerServer.java
@@ -1,23 +1,9 @@
 package de.rasmusantons.playertracker.server;
 
-import de.rasmusantons.playertracker.Utils;
-import de.rasmusantons.playertracker.network.c2s.TrackPlayerPacket;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
 
 public class PlayerTrackerServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
-        ServerPlayNetworking.registerGlobalReceiver(TrackPlayerPacket.TYPE, (payload, context) -> {
-            MinecraftServer server = context.player().getServer();
-            if (server == null)
-                return;
-            ServerPlayer targetPlayer = server.getPlayerList().getPlayer(payload.getPlayer().getId());
-            if (targetPlayer == null)
-                return;
-            Utils.setTrackedPlayer(context.player(), targetPlayer);
-        });
     }
 }
